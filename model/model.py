bf2d9baf7d12649005b81ffd191bceaabee176ec
sakurachaojun <sakurachaojun@gmail.com>
2023-03-28 18:07:34 -0600

    refine_work

--- model/model.py
+++ model/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from model.memory import DKVMN
 
+
 class MODEL(nn.Module):
 
     def __init__(self, n_question, batch_size, q_embed_dim, qa_embed_dim, memory_size, final_fc_dim):
@@ -20,16 +21,14 @@ class MODEL(nn.Module):
 
         self.predict_linear = nn.Linear(self.final_fc_dim, 1, bias=True)
 
-        # begin hint and attempt weight value
-        self.hint_linear = nn.Linear(50, self.q_embed_dim, bias=True)
-        self.hint_linear_final = nn.Linear(self.q_embed_dim, self.q_embed_dim, bias=True)
-        # end hint and attempt weight value
+        self.difficulty_linear = nn.Linear(self.q_embed_dim, self.q_embed_dim, bias=True)
+        self.difficulty_linear_final = nn.Linear(self.q_embed_dim, self.q_embed_dim, bias=True)
 
-
-
+        # init the key memory
         self.init_memory_key = nn.Parameter(torch.randn(self.memory_size, self.memory_key_state_dim))
         nn.init.kaiming_normal_(self.init_memory_key)
 
+        # init the value memory
         self.init_memory_value = nn.Parameter(torch.randn(self.memory_size, self.memory_value_state_dim))
         nn.init.kaiming_normal_(self.init_memory_value)
 
@@ -39,85 +38,77 @@ class MODEL(nn.Module):
 
         self.q_embed = nn.Embedding(self.n_question + 1, self.q_embed_dim, padding_idx=0)  # A matrix
         self.qa_embed = nn.Embedding(2 * self.n_question + 1, self.qa_embed_dim, padding_idx=0)  # B matrix
-        self.time_embed = nn.Embedding(4960, self.q_embed_dim, padding_idx=0)  # 102 for 2017 datasets
 
+        self.time_embed = nn.Embedding(4960, self.q_embed_dim, padding_idx=0)  # 102 for 2017 datasets
         self.attempt_embed = nn.Embedding(100, self.q_embed_dim, padding_idx=0)
         # hint, hint_total,
         self.hint_embed = nn.Embedding(2, self.q_embed_dim, padding_idx=0)
         self.hint_total_embed = nn.Embedding(60, self.q_embed_dim, padding_idx=0)
 
-        self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
-        self.sigmoid = nn.Sigmoid()
-
     def init_params(self):
         nn.init.kaiming_normal_(self.predict_linear.weight)
         nn.init.kaiming_normal_(self.read_embed_linear.weight)
-
         # begin hint and attempt weight value
-        nn.init.kaiming_normal_(self.hint_linear.weight)
-        nn.init.kaiming_normal_(self.hint_linear_final.weight)
+        nn.init.kaiming_normal_(self.difficulty_linear.weight)
+        nn.init.kaiming_normal_(self.difficulty_linear_final.weight)
 
         nn.init.constant_(self.read_embed_linear.bias, 0)
         nn.init.constant_(self.predict_linear.bias, 0)
+        nn.init.constant_(self.difficulty_linear.bias, 0)
+        nn.init.constant_(self.difficulty_linear_final.bias, 0)
 
-        # begin hint and attempt weight value
-        nn.init.constant_(self.hint_linear.bias,0)
-        nn.init.constant_(self.hint_linear_final.bias,0)
     def init_embeddings(self):
         nn.init.kaiming_normal_(self.q_embed.weight)
         nn.init.kaiming_normal_(self.qa_embed.weight)
-        nn.init.kaiming_normal_(self.time_embed.weight)
 
+        nn.init.kaiming_normal_(self.time_embed.weight)
         nn.init.kaiming_normal_(self.attempt_embed.weight)
         nn.init.kaiming_normal_(self.hint_embed.weight)
         nn.init.kaiming_normal_(self.hint_total_embed.weight)
+
     # new function begin here
 
     def forward(self, q_data, qa_data, target, time_data, attempt_data, hint_data, hintTotal_data):
         batch_size = q_data.shape[0]
         seqlen = q_data.shape[1]
-        q_embed_data = self.q_embed(q_data)  # +time
+        q_embed_data = self.q_embed(q_data)
 
         # begin timedata
         # time_layer = nn.Tanh()
         # time_embed_data_layer = time_layer(self.time_embed(time_data))
         # end timedata
 
-        # begin time factor plugin
+        # begin difficulty linear
         time_embed_data = self.time_embed(time_data)
-        attempt_data = self.attempt_embed(attempt_data)
-        time_attempt_plugin = time_embed_data + attempt_data
-        time_attempt_plugin_layer_data = self.tanh(time_attempt_plugin) # time factor plug-in breakpoint
-        # end time factor plugin
+        attempt_embed_data = self.attempt_embed(attempt_data)
+        hint_action_embed_data = self.hint_total_embed(hint_data)
+        hint_total_embed_data = self.hint_total_embed(hintTotal_data)
+
+        time_init_weight = self.difficulty_linear(time_embed_data + q_embed_data)
+        time_weight_update = torch.tanh(time_init_weight)
+        time_second_weight = self.difficulty_linear_final(time_weight_update)
+        time_final_weight = torch.sigmoid(time_second_weight)
+
+        attempt_init_weight = self.difficulty_linear(attempt_embed_data + q_embed_data)
+        attempt_weight_update = torch.tanh(attempt_init_weight)
+        attempt_second_weight = self.difficulty_linear_final(attempt_weight_update)
+        attempt_final_weight = torch.sigmoid(attempt_second_weight)
+
+        hint_action_init_weight = self.difficulty_linear(hint_action_embed_data + q_embed_data)
+        hint_action_weight_update = torch.tanh(hint_action_init_weight)
+        # hint_action_second_weight = self.difficulty_linear_final(hint_action_weight_update)
+        # hint_action_final_weight = torch.sigmoid(hint_action_second_weight)
 
 
-
-        # begin hint and attempt weight value
-        hintTotal_embed_data = self.hint_total_embed(hintTotal_data)
-
-        hint_total_weight = self.hint_linear(q_embed_data+hintTotal_embed_data)
-        weight_update = self.tanh(hint_total_weight)
-        weight_update_2 = self.hint_linear_final(weight_update)
-        hint_total_final = self.sigmoid(weight_update_2)
+        hint_total_init_weight = self.difficulty_linear(hint_total_embed_data + q_embed_data)
+        hint_total_weight_update = torch.tanh(hint_total_init_weight)
+        hint_total_second_weight = self.difficulty_linear_final(hint_total_weight_update)
+        hint_total_final_weight = torch.sigmoid(hint_total_second_weight)
 
 
-        # input is the hint action
-        hint_action_embed_data = self.hint_total_embed(hint_data) # embedding first
-        hint_action = self.hint_linear(q_embed_data + hint_action_embed_data) # full connect the linear network
-        hint_action_update = self.relu(hint_action)
-
-        # internal detector
-        internal_output = self.hint_linear_final(
-            hint_total_final * hintTotal_embed_data +
-            hint_action_update * hint_action_embed_data)
-
-
-        # end weight value
-
-        difficulty_total = self.tanh(internal_output + time_attempt_plugin_layer_data)
-
-        q_embed_data = q_embed_data + difficulty_total
+        q_embed_data = q_embed_data + (time_final_weight * time_embed_data) + (
+                    attempt_final_weight * attempt_embed_data) + (hint_action_weight_update * hint_action_embed_data)+(
+                    hint_total_final_weight * hint_total_embed_data)
 
         qa_embed_data = self.qa_embed(qa_data)
 
@@ -144,6 +135,7 @@ class MODEL(nn.Module):
             self.mem.write(correlation_weight, qa)
 
         all_read_value_content = torch.cat([value_read_content_l[i].unsqueeze(1) for i in range(seqlen)], 1)
+
         input_embed_content = torch.cat([input_embed_l[i].unsqueeze(1) for i in range(seqlen)], 1)
 
         predict_input = torch.cat([all_read_value_content, input_embed_content], 2)
